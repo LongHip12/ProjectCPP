1a69236a92859abe493959307845e0ac70459643
LongHip12 <longhip2012@gmail.com>
2026-06-12 20:12:42 +0700

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int age = 36;
+  string name = "Phung Thanh Do";
+  cout << "Do Mixi Age: " << age << endl;
+  cout << "Do Mixi Real Name: " << name << endl;
+
+  return 0; // End program
+}
